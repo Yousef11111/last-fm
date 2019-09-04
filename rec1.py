eff680510ed5dff82081d3945e924106230bfecf
Yousef11111 <45084384+Yousef11111@users.noreply.github.com>
2019-09-04 13:37:20 -0700

    same code on movielens

---
+++ rec1.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('NAD.csv','r') as csvin, open('NAD.tsv', 'w') as tsvout:
+    csvin = csv.reader(csvin)
+    tsvout = csv.writer(tsvout, delimiter='\t')
+
+    for row in csvin:
+        tsvout.writerow(row)
